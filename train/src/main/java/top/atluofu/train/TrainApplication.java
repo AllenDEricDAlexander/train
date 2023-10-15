f122046dac28dc10c6618a69957028c021ed28c2
luofu <mqa010225@163.com>
2023-10-15 11:52:27 +0800

    12306 mini repo create

---
+++ train/src/main/java/top/atluofu/train/TrainApplication.java
@@ -0,0 +1,21 @@
+package top.atluofu.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author      有罗敷的马同学
+ * @description 12306 mini
+ * @Date        11:51 2023-10-15
+ * @Param       void
+ * @Version     v1.0
+ * @Return            void
+ **/
+@SpringBootApplication
+public class TrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainApplication.class, args);
+    }
+
+}
