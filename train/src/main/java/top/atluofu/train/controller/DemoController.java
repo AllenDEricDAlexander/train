f93e3b60cca836d388da26801b2d08382ac4901c
luofu <mqa010225@163.com>
2023-10-15 12:16:10 +0800

    default configuration

---
+++ train/src/main/java/top/atluofu/train/controller/DemoController.java
@@ -0,0 +1,21 @@
+package top.atluofu.train.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: DemoController
+ * @description: TODO
+ * @author: 有罗敷的马同学
+ * @datetime: 2023Year-10Month-15Day-12:00
+ * @Version: 1.0
+ */
+@RestController
+@RequestMapping("demo")
+public class DemoController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world12306";
+    }
+}
