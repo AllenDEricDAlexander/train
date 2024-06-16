2e7a6251d75798e41eaf9095e337528a5a12ac0d
luofu <mqa010225@163.com>
2024-06-16 15:15:11 +0800

    <feature>(<init>): <train be>
    blog
      - log file
      - git configuration

--- train/member/src/main/java/top/atluofu/train/member/MemberApplication.java
+++ train/member/src/main/java/top/atluofu/train/member/MemberApplication.java
@@ -3,6 +3,7 @@ package top.atluofu.train.member;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -25,6 +26,8 @@ public class MemberApplication {
 
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(MemberApplication.class);
+        // 如果你不想启用banner，可以使用以下的设置
+        // springApplication.setBannerMode(Banner.Mode.OFF);
         ConfigurableEnvironment environment = springApplication.run(args).getEnvironment();
         log.info("Member Application is running!");
         log.info("Address:\thttp://127.0.0.1:{}", environment.getProperty("server.port"));
