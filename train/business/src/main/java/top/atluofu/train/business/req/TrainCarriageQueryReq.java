a6825d4a7c4ace84190f01da6f0a18cd201098be
luofu <mqa010225@163.com>
2024-06-20 17:52:43 +0800

    <feature>(<pr>): <train be>
    blog
      - train_station business model
      - train_carriage business model

---
+++ train/business/src/main/java/top/atluofu/train/business/req/TrainCarriageQueryReq.java
@@ -0,0 +1,12 @@
+package top.atluofu.train.business.req;
+
+import top.atluofu.train.common.req.PageReq;
+
+public class TrainCarriageQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "TrainCarriageQueryReq{" +
+                "} " + super.toString();
+    }
+}
