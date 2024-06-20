33520783cac29ca34ea0991ea524f8b96e9f9cdb
luofu <mqa010225@163.com>
2024-06-20 16:57:04 +0800

    <feature>(<pr>): <train be fe>
    blog
      - business model init

---
+++ train/business/src/main/java/top/atluofu/train/business/BusinessApplication.java
@@ -0,0 +1,30 @@
+package top.atluofu.train.business;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * @ClassName: BusinessApplication
+ * @description: business model
+ * @author: 有罗敷的马同学
+ * @datetime: 2024Year-06Month-20Day-16:50
+ * @Version: 1.0
+ */
+
+@SpringBootApplication
+@MapperScan("top.atluofu.train.business.mapper")
+@ComponentScan("top.atluofu.train")
+@Slf4j
+public class BusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(BusinessApplication.class);
+        ConfigurableEnvironment environment = springApplication.run(args).getEnvironment();
+        log.info("Member Application is running!");
+        log.info("Address:\thttp://127.0.0.1:{}", environment.getProperty("server.port"));
+    }
+}
