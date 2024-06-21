61bb0be80559bdf46f1a9eb0962e4bea7627fe35
luofu <mqa010225@163.com>
2024-06-21 14:31:58 +0800

    <feature>(<pr>): <train be>
    blog
      - daily train service

--- train/business/src/main/java/top/atluofu/train/business/req/DailyTrainQueryReq.java
+++ train/business/src/main/java/top/atluofu/train/business/req/DailyTrainQueryReq.java
@@ -1,12 +1,38 @@
 package top.atluofu.train.business.req;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import top.atluofu.train.common.req.PageReq;
 
+import java.util.Date;
+
 public class DailyTrainQueryReq extends PageReq {
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    private String code;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 
     @Override
     public String toString() {
         return "DailyTrainQueryReq{" +
+                "date=" + date +
+                ", code='" + code + '\'' +
                 "} " + super.toString();
     }
 }
--- train/business/src/main/java/top/atluofu/train/business/service/DailyTrainService.java
+++ train/business/src/main/java/top/atluofu/train/business/service/DailyTrainService.java
@@ -1,32 +1,38 @@
 package top.atluofu.train.business.service;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.ObjectUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import top.atluofu.train.common.resp.PageResp;
-import top.atluofu.train.common.util.SnowUtil;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import top.atluofu.train.business.domain.DailyTrain;
 import top.atluofu.train.business.domain.DailyTrainExample;
+import top.atluofu.train.business.domain.Train;
 import top.atluofu.train.business.mapper.DailyTrainMapper;
 import top.atluofu.train.business.req.DailyTrainQueryReq;
 import top.atluofu.train.business.req.DailyTrainSaveReq;
 import top.atluofu.train.business.resp.DailyTrainQueryResp;
-import jakarta.annotation.Resource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
+import top.atluofu.train.common.resp.PageResp;
+import top.atluofu.train.common.util.SnowUtil;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
+@Slf4j
 public class DailyTrainService {
-
-    private static final Logger LOG = LoggerFactory.getLogger(DailyTrainService.class);
 
     @Resource
     private DailyTrainMapper dailyTrainMapper;
+
+    @Resource
+    private TrainService trainService;
 
     public void save(DailyTrainSaveReq req) {
         DateTime now = DateTime.now();
@@ -44,17 +50,23 @@ public class DailyTrainService {
 
     public PageResp<DailyTrainQueryResp> queryList(DailyTrainQueryReq req) {
         DailyTrainExample dailyTrainExample = new DailyTrainExample();
-        dailyTrainExample.setOrderByClause("id desc");
+        dailyTrainExample.setOrderByClause("date desc, code asc");
         DailyTrainExample.Criteria criteria = dailyTrainExample.createCriteria();
+        if (ObjectUtil.isNotNull(req.getDate())) {
+            criteria.andDateEqualTo(req.getDate());
+        }
+        if (ObjectUtil.isNotEmpty(req.getCode())) {
+            criteria.andCodeEqualTo(req.getCode());
+        }
 
-        LOG.info("查询页码：{}", req.getPage());
-        LOG.info("每页条数：{}", req.getSize());
+        log.info("查询页码：{}", req.getPage());
+        log.info("每页条数：{}", req.getSize());
         PageHelper.startPage(req.getPage(), req.getSize());
         List<DailyTrain> dailyTrainList = dailyTrainMapper.selectByExample(dailyTrainExample);
 
         PageInfo<DailyTrain> pageInfo = new PageInfo<>(dailyTrainList);
-        LOG.info("总行数：{}", pageInfo.getTotal());
-        LOG.info("总页数：{}", pageInfo.getPages());
+        log.info("总行数：{}", pageInfo.getTotal());
+        log.info("总页数：{}", pageInfo.getPages());
 
         List<DailyTrainQueryResp> list = BeanUtil.copyToList(dailyTrainList, DailyTrainQueryResp.class);
 
@@ -66,5 +78,44 @@ public class DailyTrainService {
 
     public void delete(Long id) {
         dailyTrainMapper.deleteByPrimaryKey(id);
+    }
+
+    /**
+     * 生成某日所有车次信息，包括车次、车站、车厢、座位
+     *
+     * @param date
+     */
+    public void genDaily(Date date) {
+        List<Train> trainList = trainService.selectAll();
+        if (CollUtil.isEmpty(trainList)) {
+            log.info("没有车次基础数据，任务结束");
+            return;
+        }
+
+        for (Train train : trainList) {
+            genDailyTrain(date, train);
+        }
+    }
+
+    @Transactional
+    public void genDailyTrain(Date date, Train train) {
+        log.info("生成日期【{}】车次【{}】的信息开始", DateUtil.formatDate(date), train.getCode());
+        // 删除该车次已有的数据
+        DailyTrainExample dailyTrainExample = new DailyTrainExample();
+        dailyTrainExample.createCriteria()
+                .andDateEqualTo(date)
+                .andCodeEqualTo(train.getCode());
+        dailyTrainMapper.deleteByExample(dailyTrainExample);
+
+        // 生成该车次的数据
+        DateTime now = DateTime.now();
+        DailyTrain dailyTrain = BeanUtil.copyProperties(train, DailyTrain.class);
+        dailyTrain.setId(SnowUtil.getSnowflakeNextId());
+        dailyTrain.setCreateTime(now);
+        dailyTrain.setUpdateTime(now);
+        dailyTrain.setDate(date);
+        dailyTrainMapper.insert(dailyTrain);
+
+        log.info("生成日期【{}】车次【{}】的信息结束", DateUtil.formatDate(date), train.getCode());
     }
 }
