48cee267529a3be236a4b4b721c994b5246d1e4f
luofu <mqa010225@163.com>
2024-06-21 14:44:06 +0800

    <feature>(<pr>): <train be>
    blog
      - daily train seat service

---
+++ train/business/src/main/java/top/atluofu/train/business/req/DailyTrainSeatQueryReq.java
@@ -0,0 +1,12 @@
+package top.atluofu.train.business.req;
+
+import top.atluofu.train.common.req.PageReq;
+
+public class DailyTrainSeatQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "DailyTrainSeatQueryReq{" +
+                "} " + super.toString();
+    }
+}
