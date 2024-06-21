0c4f13d08dd83c8e02c3008d3b68528453ab8910
luofu <mqa010225@163.com>
2024-06-21 14:40:23 +0800

    <feature>(<pr>): <train fe>
    blog
      - daily train carriage service

--- admin/src/views/main/business/daily-train-station.vue
+++ admin/src/views/main/business/daily-train-station.vue
@@ -1,8 +1,15 @@
 <template>
   <p>
     <a-space>
-      <a-button type="primary" @click="handleQuery()">刷新</a-button>
-      <a-button type="primary" @click="onAdd">新增</a-button>
+      <a-date-picker v-model:value="params.date"
+                     valueFormat="YYYY-MM-DD"
+                     placeholder="请选择日期" />
+      <train-select-view v-model="params.trainCode"
+                         width="200px"></train-select-view>
+      <a-button type="primary"
+                @click="handleQuery()">刷新</a-button>
+      <a-button type="primary"
+                @click="onAdd">新增</a-button>
     </a-space>
   </p>
   <a-table :dataSource="dailyTrainStations"
@@ -13,10 +20,10 @@
     <template #bodyCell="{ column, record }">
       <template v-if="column.dataIndex === 'operation'">
         <a-space>
-          <a-popconfirm
-              title="删除后不可恢复，确认删除?"
-              @confirm="onDelete(record)"
-              ok-text="确认" cancel-text="取消">
+          <a-popconfirm title="删除后不可恢复，确认删除?"
+                        @confirm="onDelete(record)"
+                        ok-text="确认"
+                        cancel-text="取消">
             <a style="color: red">删除</a>
           </a-popconfirm>
           <a @click="onEdit(record)">编辑</a>
@@ -24,14 +31,21 @@
       </template>
     </template>
   </a-table>
-  <a-modal v-model:visible="visible" title="每日车站" @ok="handleOk"
-           ok-text="确认" cancel-text="取消">
-    <a-form :model="dailyTrainStation" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
+  <a-modal v-model:visible="visible"
+           title="每日车站"
+           @ok="handleOk"
+           ok-text="确认"
+           cancel-text="取消">
+    <a-form :model="dailyTrainStation"
+            :label-col="{span: 4}"
+            :wrapper-col="{ span: 20 }">
       <a-form-item label="日期">
-        <a-date-picker v-model:value="dailyTrainStation.date" valueFormat="YYYY-MM-DD" placeholder="请选择日期" />
+        <a-date-picker v-model:value="dailyTrainStation.date"
+                       valueFormat="YYYY-MM-DD"
+                       placeholder="请选择日期" />
       </a-form-item>
       <a-form-item label="车次编号">
-        <a-input v-model:value="dailyTrainStation.trainCode" />
+        <train-select-view v-model="dailyTrainStation.trainCode"></train-select-view>
       </a-form-item>
       <a-form-item label="站序">
         <a-input v-model:value="dailyTrainStation.index" />
@@ -40,16 +54,24 @@
         <a-input v-model:value="dailyTrainStation.name" />
       </a-form-item>
       <a-form-item label="站名拼音">
-        <a-input v-model:value="dailyTrainStation.namePinyin" />
+        <a-input v-model:value="dailyTrainStation.namePinyin"
+                 disabled />
       </a-form-item>
       <a-form-item label="进站时间">
-        <a-time-picker v-model:value="dailyTrainStation.inTime" valueFormat="HH:mm:ss" placeholder="请选择时间" />
+        <a-time-picker v-model:value="dailyTrainStation.inTime"
+                       valueFormat="HH:mm:ss"
+                       placeholder="请选择时间" />
       </a-form-item>
       <a-form-item label="出站时间">
-        <a-time-picker v-model:value="dailyTrainStation.outTime" valueFormat="HH:mm:ss" placeholder="请选择时间" />
+        <a-time-picker v-model:value="dailyTrainStation.outTime"
+                       valueFormat="HH:mm:ss"
+                       placeholder="请选择时间" />
       </a-form-item>
       <a-form-item label="停站时长">
-        <a-time-picker v-model:value="dailyTrainStation.stopTime" valueFormat="HH:mm:ss" placeholder="请选择时间" />
+        <a-time-picker v-model:value="dailyTrainStation.stopTime"
+                       valueFormat="HH:mm:ss"
+                       placeholder="请选择时间"
+                       disabled />
       </a-form-item>
       <a-form-item label="里程（公里）">
         <a-input v-model:value="dailyTrainStation.km" />
@@ -59,13 +81,17 @@
 </template>
 
 <script>
-import { defineComponent, ref, onMounted } from 'vue';
-import {notification} from "ant-design-vue";
+import { defineComponent, ref, onMounted, watch } from 'vue';
+import { notification } from "ant-design-vue";
 import axios from "axios";
+import { pinyin } from "pinyin-pro";
+import dayjs from "dayjs";
+import TrainSelectView from "@/components/train-select";
 
 export default defineComponent({
   name: "daily-train-station-view",
-  setup() {
+  components: { TrainSelectView },
+  setup () {
     const visible = ref(false);
     let dailyTrainStation = ref({
       id: undefined,
@@ -89,57 +115,80 @@ export default defineComponent({
       pageSize: 10,
     });
     let loading = ref(false);
+    let params = ref({
+      trainCode: null,
+      date: null
+    });
     const columns = [
-    {
-      title: '日期',
-      dataIndex: 'date',
-      key: 'date',
-    },
-    {
-      title: '车次编号',
-      dataIndex: 'trainCode',
-      key: 'trainCode',
-    },
-    {
-      title: '站序',
-      dataIndex: 'index',
-      key: 'index',
-    },
-    {
-      title: '站名',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: '站名拼音',
-      dataIndex: 'namePinyin',
-      key: 'namePinyin',
-    },
-    {
-      title: '进站时间',
-      dataIndex: 'inTime',
-      key: 'inTime',
-    },
-    {
-      title: '出站时间',
-      dataIndex: 'outTime',
-      key: 'outTime',
-    },
-    {
-      title: '停站时长',
-      dataIndex: 'stopTime',
-      key: 'stopTime',
-    },
-    {
-      title: '里程（公里）',
-      dataIndex: 'km',
-      key: 'km',
-    },
-    {
-      title: '操作',
-      dataIndex: 'operation'
-    }
+      {
+        title: '日期',
+        dataIndex: 'date',
+        key: 'date',
+      },
+      {
+        title: '车次编号',
+        dataIndex: 'trainCode',
+        key: 'trainCode',
+      },
+      {
+        title: '站序',
+        dataIndex: 'index',
+        key: 'index',
+      },
+      {
+        title: '站名',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '站名拼音',
+        dataIndex: 'namePinyin',
+        key: 'namePinyin',
+      },
+      {
+        title: '进站时间',
+        dataIndex: 'inTime',
+        key: 'inTime',
+      },
+      {
+        title: '出站时间',
+        dataIndex: 'outTime',
+        key: 'outTime',
+      },
+      {
+        title: '停站时长',
+        dataIndex: 'stopTime',
+        key: 'stopTime',
+      },
+      {
+        title: '里程（公里）',
+        dataIndex: 'km',
+        key: 'km',
+      },
+      {
+        title: '操作',
+        dataIndex: 'operation'
+      }
     ];
+    watch(() => dailyTrainStation.value.name, () => {
+      if (Tool.isNotEmpty(dailyTrainStation.value.name)) {
+        dailyTrainStation.value.namePinyin = pinyin(dailyTrainStation.value.name, { toneType: 'none' }).replaceAll(" ", "");
+      } else {
+        dailyTrainStation.value.namePinyin = "";
+      }
+    }, { immediate: true });
+
+    // 自动计算停车时长
+    watch(() => dailyTrainStation.value.inTime, () => {
+      let diff = dayjs(dailyTrainStation.value.outTime, 'HH:mm:ss').diff(dayjs(dailyTrainStation.value.inTime, 'HH:mm:ss'), 'seconds');
+      dailyTrainStation.value.stopTime = dayjs('00:00:00', 'HH:mm:ss').second(diff).format('HH:mm:ss');
+    }, { immediate: true });
+
+    // 自动计算停车时长
+    watch(() => dailyTrainStation.value.outTime, () => {
+      let diff = dayjs(dailyTrainStation.value.outTime, 'HH:mm:ss').diff(dayjs(dailyTrainStation.value.inTime, 'HH:mm:ss'), 'seconds');
+      dailyTrainStation.value.stopTime = dayjs('00:00:00', 'HH:mm:ss').second(diff).format('HH:mm:ss');
+    }, { immediate: true });
 
     const onAdd = () => {
       dailyTrainStation.value = {};
@@ -155,13 +204,13 @@ export default defineComponent({
       axios.delete("/business/admin/daily-train-station/delete/" + record.id).then((response) => {
         const data = response.data;
         if (data.success) {
-          notification.success({description: "删除成功！"});
+          notification.success({ description: "删除成功！" });
           handleQuery({
             page: pagination.value.current,
             size: pagination.value.pageSize,
           });
         } else {
-          notification.error({description: data.message});
+          notification.error({ description: data.message });
         }
       });
     };
@@ -170,14 +219,14 @@ export default defineComponent({
       axios.post("/business/admin/daily-train-station/save", dailyTrainStation.value).then((response) => {
         let data = response.data;
         if (data.success) {
-          notification.success({description: "保存成功！"});
+          notification.success({ description: "保存成功！" });
           visible.value = false;
           handleQuery({
             page: pagination.value.current,
             size: pagination.value.pageSize
           });
         } else {
-          notification.error({description: data.message});
+          notification.error({ description: data.message });
         }
       });
     };
@@ -193,7 +242,9 @@ export default defineComponent({
       axios.get("/business/admin/daily-train-station/query-list", {
         params: {
           page: param.page,
-          size: param.size
+          size: param.size,
+          trainCode: params.value.trainCode,
+          date: params.value.date
         }
       }).then((response) => {
         loading.value = false;
@@ -204,17 +255,16 @@ export default defineComponent({
           pagination.value.current = param.page;
           pagination.value.total = data.content.total;
         } else {
-          notification.error({description: data.message});
+          notification.error({ description: data.message });
         }
       });
     };
 
-    const handleTableChange = (page) => {
-      // console.log("看看自带的分页参数都有啥：" + JSON.stringify(page));
-      pagination.value.pageSize = page.pageSize;
+    const handleTableChange = (pagination) => {
+      // console.log("看看自带的分页参数都有啥：" + pagination);
       handleQuery({
-        page: page.current,
-        size: page.pageSize
+        page: pagination.current,
+        size: pagination.pageSize
       });
     };
 
@@ -237,7 +287,8 @@ export default defineComponent({
       onAdd,
       handleOk,
       onEdit,
-      onDelete
+      onDelete,
+      params
     };
   },
 });
--- admin/src/views/main/business/daily-train.vue
+++ admin/src/views/main/business/daily-train.vue
@@ -1,11 +1,17 @@
 <template>
   <p>
     <a-space>
-      <a-date-picker v-model:value="params.date" valueFormat="YYYY-MM-DD" placeholder="请选择日期" />
-      <train-select-view v-model="params.code" width="200px"></train-select-view>
-      <a-button type="primary" @click="handleQuery()">刷新</a-button>
-      <a-button type="primary" @click="onAdd">新增</a-button>
-      <a-button type="danger" @click="onClickGenDaily">手动生成车次信息</a-button>
+      <a-date-picker v-model:value="params.date"
+                     valueFormat="YYYY-MM-DD"
+                     placeholder="请选择日期" />
+      <train-select-view v-model="params.code"
+                         width="200px"></train-select-view>
+      <a-button type="primary"
+                @click="handleQuery()">刷新</a-button>
+      <a-button type="primary"
+                @click="onAdd">新增</a-button>
+      <a-button type="danger"
+                @click="onClickGenDaily">手动生成车次信息</a-button>
     </a-space>
   </p>
   <a-table :dataSource="dailyTrains"
@@ -16,17 +22,18 @@
     <template #bodyCell="{ column, record }">
       <template v-if="column.dataIndex === 'operation'">
         <a-space>
-          <a-popconfirm
-              title="删除后不可恢复，确认删除?"
-              @confirm="onDelete(record)"
-              ok-text="确认" cancel-text="取消">
+          <a-popconfirm title="删除后不可恢复，确认删除?"
+                        @confirm="onDelete(record)"
+                        ok-text="确认"
+                        cancel-text="取消">
             <a style="color: red">删除</a>
           </a-popconfirm>
           <a @click="onEdit(record)">编辑</a>
         </a-space>
       </template>
       <template v-else-if="column.dataIndex === 'type'">
-        <span v-for="item in TRAIN_TYPE_ARRAY" :key="item.code">
+        <span v-for="item in TRAIN_TYPE_ARRAY"
+              :key="item.code">
           <span v-if="item.code === record.type">
             {{item.desc}}
           </span>
@@ -34,18 +41,28 @@
       </template>
     </template>
   </a-table>
-  <a-modal v-model:visible="visible" title="每日车次" @ok="handleOk"
-           ok-text="确认" cancel-text="取消">
-    <a-form :model="dailyTrain" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
+  <a-modal v-model:visible="visible"
+           title="每日车次"
+           @ok="handleOk"
+           ok-text="确认"
+           cancel-text="取消">
+    <a-form :model="dailyTrain"
+            :label-col="{span: 4}"
+            :wrapper-col="{ span: 20 }">
       <a-form-item label="日期">
-        <a-date-picker v-model:value="dailyTrain.date" valueFormat="YYYY-MM-DD" placeholder="请选择日期" />
+        <a-date-picker v-model:value="dailyTrain.date"
+                       valueFormat="YYYY-MM-DD"
+                       placeholder="请选择日期" />
       </a-form-item>
       <a-form-item label="车次编号">
-        <train-select-view v-model="dailyTrain.code" @change="onChangeCode"></train-select-view>
+        <train-select-view v-model="dailyTrain.code"
+                           @change="onChangeCode"></train-select-view>
       </a-form-item>
       <a-form-item label="车次类型">
         <a-select v-model:value="dailyTrain.type">
-          <a-select-option v-for="item in TRAIN_TYPE_ARRAY" :key="item.code" :value="item.code">
+          <a-select-option v-for="item in TRAIN_TYPE_ARRAY"
+                           :key="item.code"
+                           :value="item.code">
             {{item.desc}}
           </a-select-option>
         </a-select>
@@ -57,7 +74,9 @@
         <a-input v-model:value="dailyTrain.startPinyin" />
       </a-form-item>
       <a-form-item label="出发时间">
-        <a-time-picker v-model:value="dailyTrain.startTime" valueFormat="HH:mm:ss" placeholder="请选择时间" />
+        <a-time-picker v-model:value="dailyTrain.startTime"
+                       valueFormat="HH:mm:ss"
+                       placeholder="请选择时间" />
       </a-form-item>
       <a-form-item label="终点站">
         <a-input v-model:value="dailyTrain.end" />
@@ -66,15 +85,24 @@
         <a-input v-model:value="dailyTrain.endPinyin" />
       </a-form-item>
       <a-form-item label="到站时间">
-        <a-time-picker v-model:value="dailyTrain.endTime" valueFormat="HH:mm:ss" placeholder="请选择时间" />
+        <a-time-picker v-model:value="dailyTrain.endTime"
+                       valueFormat="HH:mm:ss"
+                       placeholder="请选择时间" />
       </a-form-item>
     </a-form>
   </a-modal>
-  <a-modal v-model:visible="genDailyVisible" title="生成车次" @ok="handleGenDailyOk"
-           :confirm-loading="genDailyLoading" ok-text="确认" cancel-text="取消">
-    <a-form :model="genDaily" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
+  <a-modal v-model:visible="genDailyVisible"
+           title="生成车次"
+           @ok="handleGenDailyOk"
+           :confirm-loading="genDailyLoading"
+           ok-text="确认"
+           cancel-text="取消">
+    <a-form :model="genDaily"
+            :label-col="{span: 4}"
+            :wrapper-col="{ span: 20 }">
       <a-form-item label="日期">
-        <a-date-picker v-model:value="genDaily.date" placeholder="请选择日期"/>
+        <a-date-picker v-model:value="genDaily.date"
+                       placeholder="请选择日期" />
       </a-form-item>
     </a-form>
   </a-modal>
@@ -82,15 +110,15 @@
 
 <script>
 import { defineComponent, ref, onMounted } from 'vue';
-import {notification} from "ant-design-vue";
+import { notification } from "ant-design-vue";
 import axios from "axios";
 import TrainSelectView from "@/components/train-select";
 import dayjs from 'dayjs';
 
 export default defineComponent({
   name: "daily-train-view",
-  components: {TrainSelectView},
-  setup() {
+  components: { TrainSelectView },
+  setup () {
     const TRAIN_TYPE_ARRAY = window.TRAIN_TYPE_ARRAY;
     const visible = ref(false);
     let dailyTrain = ref({
@@ -124,55 +152,55 @@ export default defineComponent({
     const genDailyVisible = ref(false);
     const genDailyLoading = ref(false);
     const columns = [
-    {
-      title: '日期',
-      dataIndex: 'date',
-      key: 'date',
-    },
-    {
-      title: '车次编号',
-      dataIndex: 'code',
-      key: 'code',
-    },
-    {
-      title: '车次类型',
-      dataIndex: 'type',
-      key: 'type',
-    },
-    {
-      title: '始发站',
-      dataIndex: 'start',
-      key: 'start',
-    },
-    {
-      title: '始发站拼音',
-      dataIndex: 'startPinyin',
-      key: 'startPinyin',
-    },
-    {
-      title: '出发时间',
-      dataIndex: 'startTime',
-      key: 'startTime',
-    },
-    {
-      title: '终点站',
-      dataIndex: 'end',
-      key: 'end',
-    },
-    {
-      title: '终点站拼音',
-      dataIndex: 'endPinyin',
-      key: 'endPinyin',
-    },
-    {
-      title: '到站时间',
-      dataIndex: 'endTime',
-      key: 'endTime',
-    },
-    {
-      title: '操作',
-      dataIndex: 'operation'
-    }
+      {
+        title: '日期',
+        dataIndex: 'date',
+        key: 'date',
+      },
+      {
+        title: '车次编号',
+        dataIndex: 'code',
+        key: 'code',
+      },
+      {
+        title: '车次类型',
+        dataIndex: 'type',
+        key: 'type',
+      },
+      {
+        title: '始发站',
+        dataIndex: 'start',
+        key: 'start',
+      },
+      {
+        title: '始发站拼音',
+        dataIndex: 'startPinyin',
+        key: 'startPinyin',
+      },
+      {
+        title: '出发时间',
+        dataIndex: 'startTime',
+        key: 'startTime',
+      },
+      {
+        title: '终点站',
+        dataIndex: 'end',
+        key: 'end',
+      },
+      {
+        title: '终点站拼音',
+        dataIndex: 'endPinyin',
+        key: 'endPinyin',
+      },
+      {
+        title: '到站时间',
+        dataIndex: 'endTime',
+        key: 'endTime',
+      },
+      {
+        title: '操作',
+        dataIndex: 'operation'
+      }
     ];
 
     const onAdd = () => {
@@ -189,13 +217,13 @@ export default defineComponent({
       axios.delete("/business/admin/daily-train/delete/" + record.id).then((response) => {
         const data = response.data;
         if (data.success) {
-          notification.success({description: "删除成功！"});
+          notification.success({ description: "删除成功！" });
           handleQuery({
             page: pagination.value.current,
             size: pagination.value.pageSize,
           });
         } else {
-          notification.error({description: data.message});
+          notification.error({ description: data.message });
         }
       });
     };
@@ -204,14 +232,14 @@ export default defineComponent({
       axios.post("/business/admin/daily-train/save", dailyTrain.value).then((response) => {
         let data = response.data;
         if (data.success) {
-          notification.success({description: "保存成功！"});
+          notification.success({ description: "保存成功！" });
           visible.value = false;
           handleQuery({
             page: pagination.value.current,
             size: pagination.value.pageSize
           });
         } else {
-          notification.error({description: data.message});
+          notification.error({ description: data.message });
         }
       });
     };
@@ -240,7 +268,7 @@ export default defineComponent({
           pagination.value.current = param.page;
           pagination.value.total = data.content.total;
         } else {
-          notification.error({description: data.message});
+          notification.error({ description: data.message });
         }
       });
     };
@@ -272,14 +300,14 @@ export default defineComponent({
         genDailyLoading.value = false;
         let data = response.data;
         if (data.success) {
-          notification.success({description: "生成成功！"});
+          notification.success({ description: "生成成功！" });
           genDailyVisible.value = false;
           handleQuery({
             page: pagination.value.current,
             size: pagination.value.pageSize
           });
         } else {
-          notification.error({description: data.message});
+          notification.error({ description: data.message });
         }
       });
     };
