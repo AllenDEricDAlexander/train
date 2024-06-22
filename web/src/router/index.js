8e9f804509956cfff028229d532686741d7a77c5
luofu <mqa010225@163.com>
2024-06-22 13:25:48 +0800

    <feature>(<pr>): <train fe>
    blog
      - confirm order

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -20,6 +20,9 @@ const routes = [{
     }, {
       path: 'ticket',
       component: () => import('../views/main/ticket.vue'),
+    }, {
+      path: 'my-ticket',
+      component: () => import('../views/main/my-ticket.vue'),
     }]
   }, {
     path: '',
